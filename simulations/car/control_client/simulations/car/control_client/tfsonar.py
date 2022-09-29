56c8f5cdb89680ea7e796927cfe79290305a5982
pieter-rotterdam <pieter.rotterdam@gmail.com>
2022-09-29 10:57:42 +0200

    working test batch

--- simulations/car/control_client/simulations/car/control_client/tfsonar.py
+++ simulations/car/control_client/simulations/car/control_client/tfsonar.py
@@ -7,29 +7,26 @@ import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Activation, Dense, BatchNormalization,  Dropout
-from tensorflow.keras.optimizers import Adam
+from tensorflow.keras import optimizers
 from tensorflow.keras.metrics import categorical_crossentropy
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.preprocessing import StandardScaler
 
-# print(f"Tensorflow version: {tf.version.VERSION}")
+print(f"Tensorflow version: {tf.version.VERSION}")
 
 d = pd.read_csv('./sonar.csv')
 d = d.astype(str)
 
 for itm in d.head():
-  d[itm] = d[itm].str.split(',')
-  
+  d[itm] = d[itm].str.split(',') 
 d = d.apply(pd.Series.explode)
-
 # d.to_csv('sonar1.csv', index=False)
 tf.random.set_seed(1)
 # this is to get same results every expiriment
 tf.debugging.set_log_device_placement(False)
 
 d.sample(frac=0.9)
-
 train_dataset = d.sample(frac=0.6)
 test_dataset = d.drop(train_dataset.index)
 train_dataset, temp_test_dataset =  train_test_split(d, test_size=0.4)
@@ -47,8 +44,11 @@ valid_labels = valid_dataset.pop('angle')
 #test normalizing vs standardizing
 #normalize
 scaler = MinMaxScaler()
+normedTrainData = scaler.fit_transform(train_dataset)
+normedTestData = scaler.fit_transform(test_dataset)
+normedValidDataset = scaler.fit_transform(valid_dataset)
 normedTrainStats = scaler.fit_transform(train_stats)
-print (normedTrainStats)
+# print (normedTrainStats)
 # normalizing helps the algorythms in the model i.e. less spiky  data
 
 #standardize
@@ -56,8 +56,26 @@ print (normedTrainStats)
 # scaler.fit_transform(d)
 # standardization gets the mean of values to 0 and the variance to 1
 
+def build_model1_one_hidden_layer():
+ model = Sequential() 
+ model.add(Dense(8, input_shape = (normedTrainData.shape[1],)))
+ model.add(Dense(32,Activation('relu'))) 
+ model.add(Dense(1)) 
+ 
+ learning_rate = 0.001
+ optimizer = optimizers.Adam(learning_rate)
+ model.compile(loss='mse',
+ optimizer=optimizer,
+ metrics=['mse']) # for regression problems, mean squared error (MSE) is often employed
+ return model
 
+model = build_model1_one_hidden_layer()
+print('Here is a summary of this model: ')
+model.summary()
 
+example_batch = normedTrainData[:10] # take the first 10 data points from the training data.
+example_result = model.predict(example_batch)
+print (example_result)
 
 # print( train_dataset.shape )
 # print( temp_test_dataset.shape )
@@ -65,16 +83,11 @@ print (normedTrainStats)
 # print( valid_dataset.shape )
 
 # print(f"Display the datatype of the test_dataset: {type(test_dataset)}")
-# print(f" Trai dataset       : {train_dataset.shape}")
+# print(f" Train dataset       : {train_dataset.shape}")
 # print(f" Test dataset       : {test_dataset.shape}")
 # print(f" Validation dataset : {valid_dataset.shape}")
-
-
-
 # print(f'No of rows/columns in the dataset: {d.shape}')
 # 1529/5
-
-train_samples=d.loc[:,'dist1':'dist3'] 
 
 # print (d.info()) 
 # print (d.dtypes)
@@ -84,48 +97,3 @@ train_samples=d.loc[:,'dist1':'dist3']
 # print (train_labels)
 # print (train_samples) 
 # print (d)
-
-# Model = tf.keras.Sequential([
-#   Dense(units=3, activation='relu'),
-#   Dense(units=1, activation='relu')])
-
-# voor ons model input = 3 of 16, de input wordt dus gegeven aan eerste hidden layer
-
-# Model.summary()
-
-# tf.convert_to_tensor(train_samples)
-''''
-# Make numpy values easier to read.
-np.set_printoptions(precision=3, suppress=True)
-
-import tensorflow as tf
-from tensorflow.keras import layers
-
-
-filename = "C:/MakeAIWork/simulations/car/control_client/sonar.csv"
-
-sonar_01_train = pd.read_csv(filename, header = 0, sep = ',')
-# print(sonar_01_train)
-# type(sonar_01_train)
-
-sonar_01_train.head()
-
-sonar_01_sensors = sonar_01_train.copy()
-sonar_01__labels = sonar_01_sensors.pop('Steering Angle')
-
-# print(sonar_01__labels)
-
-sonar_01_sensors = np.array(sonar_01_sensors)
-
-# print(sonar_01_sensors)
-
-sonar_01_model = tf.keras.Sequential([
-  layers.Dense(8),
-  layers.Dense(1)
-])
-
-sonar_01_model.compile(loss = tf.keras.losses.MeanSquaredError(),
-                      optimizer = tf.keras.optimizers.Adam(0.1))
-
-sonar_01_model.fit(sonar_01_sensors, sonar_01__labels, epochs=3)
-'''
